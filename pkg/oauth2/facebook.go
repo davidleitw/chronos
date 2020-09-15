cc13329e976fe58afbf1345c4f992291452992e8
davidlei <davidleitw@gmail.com>
2020-09-15 16:31:15 +0800

    google, facebook oauth2第三方登入架構

--- pkg/oauth2/facebook.go
+++ pkg/oauth2/facebook.go
@@ -1,9 +1,30 @@
 package oauth2
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/facebook"
+)
+
+var facebookConfig *oauth2.Config = &oauth2.Config{
+	ClientID:     "ID",
+	ClientSecret: "Secret",
+	RedirectURL:  "",
+	Scopes: []string{
+		"email",
+		"public_profile",
+	},
+	Endpoint: facebook.Endpoint,
+}
 
 func FacebookOauthLogin(ctx *gin.Context) {
+	state := xid.New().String()
+	redirectURL := googleConfig.AuthCodeURL(state)
 
+	ctx.Redirect(http.StatusSeeOther, redirectURL)
 }
 
 func FacebookCallBack(ctx *gin.Context) {
--- pkg/oauth2/google.go
+++ pkg/oauth2/google.go
@@ -1,9 +1,30 @@
 package oauth2
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var googleConfig *oauth2.Config = &oauth2.Config{
+	ClientID:     "ID",
+	ClientSecret: "Secret",
+	RedirectURL:  "",
+	Scopes: []string{
+		"https://www.googleapis.com/auth/userinfo.email",
+		"https://www.googleapis.com/auth/userinfo.profile",
+	},
+	Endpoint: google.Endpoint,
+}
 
 func GoogleOauthLogin(ctx *gin.Context) {
+	state := xid.New().String()
+	redirectURL := googleConfig.AuthCodeURL(state)
 
+	ctx.Redirect(http.StatusSeeOther, redirectURL)
 }
 
 func GoogleCallBack(ctx *gin.Context) {
